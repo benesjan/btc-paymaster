87a5992e60e6fde99d76dcd25122705670ab1807
benesjan <janbenes1234@gmail.com>
2020-05-14 17:39:29 +0200

    Request accepted only for allowed targets

--- contracts/BtcPaymaster.sol
+++ contracts/BtcPaymaster.sol
@@ -1,31 +1,43 @@
 pragma solidity ^0.6.2;
 pragma experimental ABIEncoderV2;
 
-import "@opengsn/gsn/contracts/BasePaymaster.sol";
+import '@opengsn/gsn/contracts/BasePaymaster.sol';
+
 
 contract BtcPaymaster is BasePaymaster {
     uint256 private _fee;
     address private _tokenAddress;
+    // Contracts for which the paymaster is willing to pay for
+    mapping(address => bool) _targets;
 
-    constructor(uint256 fee, address tokenAddress) Ownable() public {
+    constructor(uint256 fee, address tokenAddress) public Ownable() {
         _fee = fee;
         _tokenAddress = tokenAddress;
     }
 
-    function setFee(uint256 fee) onlyOwner public {
+    function setFee(uint256 fee) public onlyOwner {
         _fee = fee;
+    }
+
+    function addTarget(address newTarget) public onlyOwner {
+        _targets[newTarget] = true;
+    }
+
+    function removeTarget(address target) public onlyOwner {
+        _targets[target] = false;
     }
 
     function acceptRelayedCall(
         GSNTypes.RelayRequest calldata relayRequest,
         bytes calldata approvalData,
         uint256 maxPossibleGas
-    ) external view override returns (bytes memory) {
-        return "";
+    ) external override view returns (bytes memory) {
+        require(_targets[relayRequest.target], 'Address not in targets.');
+        return '';
     }
 
     function preRelayedCall(bytes calldata context) external override returns (bytes32) {
-        return "";
+        return '';
     }
 
     function postRelayedCall(
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,22 @@
 import { ethers } from '@nomiclabs/buidler';
-import { FEE, ERC20_TOKEN } from '../config';
+import { FEE, ERC20_TOKEN, TORNADO_PBTC_INSTANCES_ADDRESSES } from '../config';
 
 async function main() {
-    const factory = await ethers.getContract('BtcPaymaster');
+    const BtcPaymaster = await ethers.getContractFactory('BtcPaymaster');
+    const btcPaymaster = await BtcPaymaster.deploy(FEE, ERC20_TOKEN);
 
-    // If we had constructor arguments, they would be passed into deploy()
-    let contract = await factory.deploy(FEE, ERC20_TOKEN);
-
-    // The address the Contract WILL have once mined
-    console.log(contract.address);
-
-    // The transaction that was sent to the network to deploy the Contract
-    console.log(contract.deployTransaction.hash);
+    console.log(`Contract address: ${btcPaymaster.address}`);
+    console.log(`Tx hash: ${btcPaymaster.deployTransaction.hash}`);
 
     // The contract is NOT deployed yet; we must wait until it is mined
-    await contract.deployed();
+    await btcPaymaster.deployed();
+
+    for (let i = 0; i < TORNADO_PBTC_INSTANCES_ADDRESSES.length; i++) {
+        let instanceAddress = TORNADO_PBTC_INSTANCES_ADDRESSES[i];
+        let tx = await btcPaymaster.addTarget(instanceAddress);
+        console.log(`Adding instance ${instanceAddress} to the mapping of targets, tx hash: ${tx.hash}.`);
+        await tx.wait();
+    }
 }
 
 main()

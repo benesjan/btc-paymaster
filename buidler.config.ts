872f0f19033b6f52fe5a8a0445995f0241009950
benesjan <janbenes1234@gmail.com>
2020-05-14 11:54:55 +0200

    Bug fix of etherscan API definition, prettification

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,31 +1,33 @@
-import { BuidlerConfig, usePlugin } from "@nomiclabs/buidler/config";
-import * as dotenv from "dotenv";
+import { BuidlerConfig, usePlugin } from '@nomiclabs/buidler/config';
+import * as dotenv from 'dotenv';
 
-usePlugin("@nomiclabs/buidler-ethers");
-usePlugin("@nomiclabs/buidler-etherscan");
+usePlugin('@nomiclabs/buidler-ethers');
+usePlugin('@nomiclabs/buidler-etherscan');
 
 dotenv.config();
 
-const NETWORK: string = <string> process.env.NETWORK;
-const INFURA_PROJECT_ID: string = <string> process.env.INFURA_PROJECT_ID;
-const PRIVATE_KEY: string = <string> process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = <string> process.env.ETHERSCAN_API_KEY;
+const NETWORK: string = <string>process.env.NETWORK;
+const INFURA_PROJECT_ID: string = <string>process.env.INFURA_PROJECT_ID;
+const PRIVATE_KEY: string = <string>process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY: string = <string>process.env.ETHERSCAN_API_KEY;
+
+const ETHERSCAN_API_PREFIX: string = NETWORK === 'mainnet' ? 'api' : `api-${NETWORK}`;
 
 const config: BuidlerConfig = {
     defaultNetwork: NETWORK,
     solc: {
-        version: "0.6.7"
+        version: '0.6.7',
     },
     networks: {
         ropsten: {
             url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
-            accounts: [PRIVATE_KEY]
-        }
+            accounts: [PRIVATE_KEY],
+        },
     },
     etherscan: {
-        url: "https://api-rinkeby.etherscan.io/api",
-        apiKey: ETHERSCAN_API_KEY
-    }
+        url: `https://${ETHERSCAN_API_PREFIX}.etherscan.io/api`,
+        apiKey: ETHERSCAN_API_KEY,
+    },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,10 @@
-import { ethers } from "@nomiclabs/buidler";
-import * as dotenv from "dotenv";
+import { ethers } from '@nomiclabs/buidler';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function main() {
-    const factory = await ethers.getContract("BtcPaymaster")
+    const factory = await ethers.getContract('BtcPaymaster');
 
     // If we had constructor arguments, they would be passed into deploy()
     let contract = await factory.deploy(process.env.FEE, process.env.ERC20_TOKEN);
@@ -16,12 +16,12 @@ async function main() {
     console.log(contract.deployTransaction.hash);
 
     // The contract is NOT deployed yet; we must wait until it is mined
-    await contract.deployed()
+    await contract.deployed();
 }
 
 main()
     .then(() => process.exit(0))
-    .catch(error => {
+    .catch((error) => {
         console.error(error);
         process.exit(1);
     });

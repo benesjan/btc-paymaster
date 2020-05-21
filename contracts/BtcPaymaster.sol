a748e0a5cf3f7949e628f272a10fcbf172fd0507
benesjan <janbenes1234@gmail.com>
2020-05-21 12:35:59 +0200

    Method signature check in the paymaster

--- contracts/BtcPaymaster.sol
+++ contracts/BtcPaymaster.sol
@@ -2,17 +2,22 @@ pragma solidity ^0.6.2;
 pragma experimental ABIEncoderV2;
 
 import '@opengsn/gsn/contracts/BasePaymaster.sol';
+import '@opengsn/gsn/contracts/utils/GsnUtils.sol';
 import 'openzeppelin-solidity/contracts/token/ERC20/IERC20.sol';
+
 
 // target contract interface - Tornado
 abstract contract Target {
     uint256 public denomination;
+
+    function deposit(bytes32 _commitment) external virtual payable;
 }
+
 
 contract BtcPaymaster is BasePaymaster {
     uint256 private _fee;
     IERC20 private _token;
-    // Contracts for which the paymaster is willing to pay for and their denominations
+    // Contracts for which the paymaster is willing to pay for
     mapping(address => Target) _targets;
 
     constructor(uint256 fee, address tokenAddress) public Ownable() {
@@ -41,7 +46,8 @@ contract BtcPaymaster is BasePaymaster {
         bytes calldata approvalData,
         uint256 maxPossibleGas
     ) external override view returns (bytes memory) {
-        uint256 denomination = _targets[relayRequest.target].denomination();
+        Target target = _targets[relayRequest.target];
+        uint256 denomination = target.denomination();
         require(denomination != 0, 'Address not in targets.');
         address sender = relayRequest.relayData.senderAddress;
         require(_token.balanceOf(sender) >= (denomination + _fee), "Sender's balance is too low");
@@ -53,12 +59,14 @@ contract BtcPaymaster is BasePaymaster {
             _token.allowance(sender, address(this)) >= _fee,
             "Sender's allowance for paymaster is lower than the paymaster's fee"
         );
+        bytes4 sig = GsnUtils.getMethodSig(relayRequest.encodedFunction);
+        require(sig == target.deposit.selector, 'Only deposit function can be invoked through the relay');
         return abi.encode(sender);
     }
 
     function preRelayedCall(bytes calldata context) external override relayHubOnly returns (bytes32) {
         address sender = abi.decode(context, (address));
-        if (_fee!=0) {
+        if (_fee != 0) {
             _token.transferFrom(sender, address(this), _fee);
         }
         return bytes32(0);
